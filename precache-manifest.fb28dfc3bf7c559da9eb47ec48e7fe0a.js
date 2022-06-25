3cc1b07559c04ec9747128a18ea0b339125ce31d
Mehedi Khan Rakib <mkrakib007@gmail.com>
2022-06-26 00:02:36 +0600

    Updates

---
+++ precache-manifest.fb28dfc3bf7c559da9eb47ec48e7fe0a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b797961082a612ee8bb202e8566a2132",
+    "url": "/University-Website/index.html"
+  },
+  {
+    "revision": "713f4397e696da425a42",
+    "url": "/University-Website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0edc8df7a4ab2e344d5f",
+    "url": "/University-Website/static/css/main.da987e3b.chunk.css"
+  },
+  {
+    "revision": "713f4397e696da425a42",
+    "url": "/University-Website/static/js/2.d426aa66.chunk.js"
+  },
+  {
+    "revision": "cfe63c1848a227eaaf4c756b8cf4bf44",
+    "url": "/University-Website/static/js/2.d426aa66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edc8df7a4ab2e344d5f",
+    "url": "/University-Website/static/js/main.ee4fc2c9.chunk.js"
+  },
+  {
+    "revision": "28b9cd248211f8ca01cc",
+    "url": "/University-Website/static/js/runtime-main.81dace9e.js"
+  },
+  {
+    "revision": "eb4583ad3cfc0608bb7ecadc22bb73ce",
+    "url": "/University-Website/static/media/College Logo.eb4583ad.png"
+  },
+  {
+    "revision": "c129abfad830e4b1d7971428c673530d",
+    "url": "/University-Website/static/media/JKKNIU.c129abfa.mp4"
+  },
+  {
+    "revision": "cf1e9d68fbc2470c5571509a8bbeedcc",
+    "url": "/University-Website/static/media/clogo.cf1e9d68.png"
+  },
+  {
+    "revision": "acdcb857adf691c097f5aa6e0d243791",
+    "url": "/University-Website/static/media/course01.acdcb857.jpeg"
+  },
+  {
+    "revision": "c668bb1f78df3e299fa449d8fbb67ec8",
+    "url": "/University-Website/static/media/logo.c668bb1f.png"
+  },
+  {
+    "revision": "5cf4db134364619cc5cff75ec28c0e6a",
+    "url": "/University-Website/static/media/slide-01.5cf4db13.png"
+  },
+  {
+    "revision": "14a51498dc15c887f0411da0d5d8ad2a",
+    "url": "/University-Website/static/media/slide-02.14a51498.png"
+  },
+  {
+    "revision": "d50467a63b355589baae1bd9a3068679",
+    "url": "/University-Website/static/media/slide-03.d50467a6.png"
+  },
+  {
+    "revision": "f75c9668e5e150e41d6bd5b5c321daa6",
+    "url": "/University-Website/static/media/vc_sir.f75c9668.jpg"
+  }
+]);
